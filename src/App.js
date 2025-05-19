f197292d300f30746bc04f6266746fa91779b9e4
Edwin <lize201@mails.ucas.ac.cn>
2025-05-20 00:38:08 +0100

    feat(component): start up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        <Link to="/about">Go to About</Link>
+      </p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
 export default App;
---
+++ src/component/FoodStorageComponent.js
@@ -0,0 +1,3 @@
+const FoodStorageComponent = function () {
+
+}
