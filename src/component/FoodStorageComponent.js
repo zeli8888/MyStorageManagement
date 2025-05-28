56d7b44e21db3a82bb9207b3913839f58abead71
Edwin <lize201@mails.ucas.ac.cn>
2025-05-28 01:19:32 +0100

    formik to go

--- src/component/FoodStorageComponent.js
+++ src/component/FoodStorageComponent.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { DataGrid } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import IngredientService from '../service/IngredientService'
 
 const FoodStorageComponent = function () {
     const navigate = useNavigate();
 
     const [ingredients, setIngredients] = useState([]);
+    const [addingIngredient, setAddingIngredient] = useState(false);
 
     useEffect(() => {
         refreshIngredients();
@@ -21,13 +26,6 @@ const FoodStorageComponent = function () {
     }
 
     const updateIngredient = (data) => {
-        // let ingredient = {
-        //     ingredientId: data.id,
-        //     ingredientName: data.ingredientName,
-        //     ingredientStorage: data.ingredientStorage,
-        //     ingredientCost: data.ingredientCost,
-        //     ingredientDesc: data.ingredientDesc
-        // };
         IngredientService.updateIngredient(data.ingredientId, data).then(response => {
             refreshIngredients();
         }).catch(error => {
@@ -110,7 +108,6 @@ const FoodStorageComponent = function () {
                     },
                     columns: {
                         columnVisibilityModel: {
-                            // Hide columns status and traderName, the other columns will remain visible
                             ingredientId: false,
                         },
                     },
@@ -125,6 +122,61 @@ const FoodStorageComponent = function () {
                 disableRowSelectionOnClick
                 getRowId={(row) => row.ingredientId}
             />
+
+            <Button variant="contained" color="success"
+                onClick={() => {
+                    setAddingIngredient(true);
+                }}
+            >
+                Add
+            </Button>
+            <Modal
+                open={addingIngredient}
+                onClose={() => { setAddingIngredient(false) }}
+                aria-labelledby="modal-title"
+            >
+                <div>
+                    <Typography id="modal-title" variant="h6" component="h2">
+                        New Ingredient
+                    </Typography>
+                    <Formik
+                        onSubmit={updateIngredient}>
+                        {
+                            (formikProps) => (
+                                <>
+                                    <Modal.Body>
+                                        <Form>
+                                            <ErrorMessage name="description" component="div"
+                                                className="alert alert-warning" />
+                                            <fieldset className="form-group">
+                                                <label>Ingredient Name</label>
+                                                <Field className="form-control" type="text" name="ingredientName" required />
+                                            </fieldset>
+                                            <fieldset className="form-group">
+                                                <label>Storage</label>
+                                                <Field className="form-control" type="number" name="ingredientStorage" />
+                                            </fieldset>
+                                            <fieldset className="form-group">
+                                                <label>Cost</label>
+                                                <Field className="form-control" type="number" name="ingredientCost" />
+                                            </fieldset>
+                                            <fieldset className="form-group">
+                                                <label>Description</label>
+                                                <Field className="form-control" type="text" name="ingredientDesc" />
+                                            </fieldset>
+                                        </Form>
+                                    </Modal.Body>
+
+                                    <Modal.Footer>
+                                        <Button variant="contained" color="alert" onClick={() => { setAddingIngredient(false) }}>Cancel</Button>
+                                        <Button variant="contained" color="success" onClick={formikProps.submitForm}>Save</Button>
+                                    </Modal.Footer>
+                                </>
+                            )
+                        }
+                    </Formik>
+                </div>
+            </Modal>
         </div>
     )
 }
